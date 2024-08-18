e3819a6816bb29598cd1a0a9047a0b6db2ec54d3
Pablo La Torre <latorrepablo775@gmail.com>
2024-08-18 16:14:45 -0400

    requerimientos completos

--- src/form.js
+++ src/form.js
@@ -3,13 +3,15 @@ document.addEventListener("DOMContentLoaded", function() {
     const usuario = {
         nombre: null,
         sexo: null,
-        edad: null
+        edad: null,
+        idioma: null
     };
     
     // Llena el formulario automáticamente
     document.getElementById("nombre").value = usuario.nombre || "";
     document.getElementById("sexo").value = usuario.sexo || "";
     document.getElementById("edad").value = usuario.edad || "";
+    document.getElementById("idioma").value = usuario.idioma || "";
 
     // Escucha el evento de envío del formulario
     document.getElementById("wellcome-Form").addEventListener("submit", function(event) {
@@ -17,8 +19,62 @@ document.addEventListener("DOMContentLoaded", function() {
         
         // Obtiene el nombre ingresado
         const nombreIngresado = document.getElementById("nombre").value;
-
+        const sexoIngresado = document.getElementById("sexo").value;
+        const edadIngresada = document.getElementById("edad").value;
+        const idiomaIngresado = document.getElementById("idioma").value;
         // Muestra el saludo de bienvenida
-        alert("Bienvenido, " + nombreIngresado + "!");
+        if(idiomaIngresado == "Español")
+        {
+            if(sexoIngresado == "Masculino")
+            {
+                if(edadIngresada<30)
+                {
+                    saludo = "Bienvenido joven "
+                }
+                else
+                {
+                    saludo = "Bienvenidopppp señor "
+                }
+            }
+            else
+            {
+                if(edadIngresada<30)
+                {
+                    saludo = "Bienvenida señorita "
+                }
+                else
+                {
+                    saludo = "Bienvenida señora "
+                }
+            }
+            mensaje = " a la pagina de la Cato"
+        }
+        else
+        {
+            if(sexoIngresado == "Masculino")
+            {
+                if(edadIngresada<30)
+                {
+                    saludo = "Welcome young "
+                }
+                else
+                {
+                    saludo = "Welcoem sir "
+                }
+            }
+            else
+            {
+                if(edadIngresada<30)
+                {
+                    saludo = "Welcome miss "
+                }
+                else
+                {
+                    saludo = "Welcoem madam "
+                }
+            }
+              mensaje = " to the Cato page"
+        }
+        alert(saludo + nombreIngresado +  mensaje)
     });
 });
